ef318294a96ea47b7bbaa769134e0397e449e569
Anna Yakovleva <_bel4onok_@mail.ru>
2021-08-14 21:52:37 +0300

    Create program bonus  calculation

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        
+        double start = 50.67;
+        long amount = 1200;
+        long bonus = 0;
+
+        if (amount > 1000) {
+            bonus = amount / 100;
+        }
+
+        double total = start + amount + bonus;
+
+        System.out.println("Total: " + total);
+        System.out.println("Bonus: " + bonus);
     }
 }
